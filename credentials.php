ff240409fc139080b1743e78204a3c005284a701
Bgdbj <167567553+Bgdbj@users.noreply.github.com>
2024-08-04 22:00:11 +0300

    new commit

---
+++ credentials.php
@@ -0,0 +1,7 @@
+<?php
+// credentials.php
+
+$credentials = [
+    'token'=>'ISSecretKey_live_d55dc00f-9eec-420a-b8f4-7d56ae4e6895',
+    'publishable_key'=>'ISPubKey_live_5c5fee06-5c92-4b13-b737-f3c36ad241af'
+];
